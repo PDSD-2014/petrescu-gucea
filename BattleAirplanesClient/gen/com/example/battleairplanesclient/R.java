748e24f8971b803516a5e088b124e4120c78036f
Rares <rares1991petrescu@yahoo.com>
2014-05-10 20:27:48 +0300

    Added HomeScreen and Game Screen

--- BattleAirplanesClient/gen/com/example/battleairplanesclient/R.java
+++ BattleAirplanesClient/gen/com/example/battleairplanesclient/R.java
@@ -23,22 +23,80 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int logo=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080001;
+        public static final int ButStartGame=0x7f08002b;
+        public static final int MessageToEntertext=0x7f080029;
+        public static final int TextIpFriend=0x7f08002a;
+        public static final int TextTeam=0x7f080001;
+        public static final int action_settings=0x7f08002c;
+        public static final int but1=0x7f080002;
+        public static final int but10=0x7f08000b;
+        public static final int but11=0x7f08000c;
+        public static final int but12=0x7f08000d;
+        public static final int but13=0x7f08000e;
+        public static final int but14=0x7f08000f;
+        public static final int but15=0x7f080010;
+        public static final int but16=0x7f080011;
+        public static final int but17=0x7f080012;
+        public static final int but18=0x7f080013;
+        public static final int but19=0x7f080014;
+        public static final int but2=0x7f080003;
+        public static final int but20=0x7f080015;
+        public static final int but21=0x7f080016;
+        public static final int but22=0x7f080017;
+        public static final int but23=0x7f080018;
+        public static final int but24=0x7f080019;
+        public static final int but25=0x7f08001a;
+        public static final int but26=0x7f08001b;
+        public static final int but27=0x7f08001c;
+        public static final int but28=0x7f08001d;
+        public static final int but29=0x7f08001e;
+        public static final int but3=0x7f080004;
+        public static final int but30=0x7f08001f;
+        public static final int but31=0x7f080020;
+        public static final int but32=0x7f080021;
+        public static final int but33=0x7f080022;
+        public static final int but34=0x7f080023;
+        public static final int but35=0x7f080024;
+        public static final int but36=0x7f080025;
+        public static final int but4=0x7f080005;
+        public static final int but5=0x7f080006;
+        public static final int but6=0x7f080007;
+        public static final int but7=0x7f080008;
+        public static final int but8=0x7f080009;
+        public static final int but9=0x7f08000a;
         public static final int container=0x7f080000;
+        public static final int logo=0x7f080028;
+        public static final int scor_juc=0x7f080026;
+        public static final int scor_opp=0x7f080027;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
-        public static final int fragment_main=0x7f030001;
+        public static final int activity_game=0x7f030000;
+        public static final int activity_game_over=0x7f030001;
+        public static final int activity_main=0x7f030002;
+        public static final int fragment_game=0x7f030003;
+        public static final int fragment_game_over=0x7f030004;
+        public static final int fragment_main=0x7f030005;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int game=0x7f070000;
+        public static final int game_over=0x7f070001;
+        public static final int main=0x7f070002;
     }
     public static final class string {
+        public static final int Team=0x7f050005;
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050001;
+        public static final int opp_score=0x7f05000a;
+        public static final int opponent_ip_complete_message=0x7f050006;
+        public static final int opponent_turn=0x7f050008;
+        public static final int title_activity_game=0x7f050003;
+        public static final int title_activity_game_over=0x7f050004;
+        public static final int your_score=0x7f050009;
+        public static final int your_turn=0x7f050007;
     }
     public static final class style {
         /** 
