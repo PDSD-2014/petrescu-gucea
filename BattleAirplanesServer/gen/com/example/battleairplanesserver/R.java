4b6cdfe5141478b9314800b969547d4b27e52c7f
dorugucea91 <gucea.doru@gmail.com>
2014-05-11 12:31:21 +0300

    Adaugare ecrane.

--- BattleAirplanesServer/gen/com/example/battleairplanesserver/R.java
+++ BattleAirplanesServer/gen/com/example/battleairplanesserver/R.java
@@ -23,22 +23,41 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int logo=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080001;
+        public static final int MessageToEntertext=0x7f080003;
+        public static final int TextTeam=0x7f080002;
+        public static final int action_settings=0x7f080004;
         public static final int container=0x7f080000;
+        public static final int logo=0x7f080001;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
-        public static final int fragment_main=0x7f030001;
+        public static final int activity_game=0x7f030000;
+        public static final int activity_game_over=0x7f030001;
+        public static final int activity_main=0x7f030002;
+        public static final int fragment_game=0x7f030003;
+        public static final int fragment_game_over=0x7f030004;
+        public static final int fragment_main=0x7f030005;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int game=0x7f070000;
+        public static final int game_over=0x7f070001;
+        public static final int main=0x7f070002;
     }
     public static final class string {
+        public static final int Team=0x7f050005;
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050001;
+        public static final int opp_score=0x7f05000a;
+        public static final int opponent_ip_complete_message=0x7f050006;
+        public static final int opponent_turn=0x7f050008;
+        public static final int title_activity_game=0x7f050003;
+        public static final int title_activity_game_over=0x7f050004;
+        public static final int waiting=0x7f05000b;
+        public static final int your_score=0x7f050009;
+        public static final int your_turn=0x7f050007;
     }
     public static final class style {
         /** 
